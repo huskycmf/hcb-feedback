811d075217976c21d121508c6a5dc6e38eb6aef6
Oleksii Mylotskyi <spalaxinco@gmail.com>
2015-01-05 00:13:16 +0200

    Remove Doctrine Entities support from this module, because it is used Entities from hcf module.

--- config/module.config.php
+++ config/module.config.php
@@ -3,19 +3,6 @@ return array(
     'router' => include __DIR__ . '/module/router.config.php',
     'di' => include __DIR__ . '/module/di.config.php',
 
-    'doctrine' => array(
-        'driver' => array(
-            'app_driver' => array(
-                'paths' => array(__DIR__ . '/../src/HcbFeedback/Entity')
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    'HcfFeedback\Entity' => 'app_driver'
-                )
-            )
-        )
-    ),
-
     'asset_manager' => array(
         'resolver_configs' => array(
             'paths' => array(
